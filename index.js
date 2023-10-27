da31db2b9a589cb84f918ded04fd9dfb8270de6d
brittmouse <bstancoff@proton.me>
2023-10-26 22:11:39 -0400

    Add JS computer choice and play round functions
    This sets up the functions to be used in the game function later.
    Certain bugs like misinputs from the user have not been added yet.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-function main() {
-  console.log("Test");
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
+  const listIndex = Math.floor(Math.random() * 3);
+  return choices[listIndex];
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else {
+    if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper') ||
+      (playerSelection === 'paper' && computerSelection === 'rock')
+    ) {
+      return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+  }
+}
+
+let playerSelection = prompt("Enter rock, paper, or scissors:").toLowerCase();
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
