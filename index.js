bb9078710c5bd96b2fa1d1e702330df5d1550503
brittmouse <bstancoff@proton.me>
2023-10-31 17:20:00 -0400

    Add interactive buttons to html, buttons console log output
    TODO: Add html body output paras, rework game() to use GUI inputs

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ function getComputerChoice() {
   return choices[listIndex];
 }
 
+const buttons = document.getElementById("buttons");
+buttons.addEventListener('click', (event) => {
+  const target = event.target;
+  switch (target.id) {
+    case "rock":
+      console.log("Rock was selected");
+      break;
+    case "paper":
+      console.log("Paper was selected");
+      break;
+    case "scissors":
+      console.log("Scissors was selected");
+      break;
+  }
+});
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return `It's a tie! You both played ${playerSelection}`;
@@ -21,26 +37,26 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  for (let i = 1; i <= 5; i++) {
-    let playerSelection = prompt("Enter rock, paper, or scissors:").toLowerCase();
-    let computerSelection = getComputerChoice();
-    while (
-      (playerSelection !== "rock") &&
-      (playerSelection !== "paper") &&
-      (playerSelection !== "scissors")
+  // for (let i = 1; i <= 5; i++) {
+  let playerSelection = prompt("Enter rock, paper, or scissors:").toLowerCase();
+  let computerSelection = getComputerChoice();
+  while (
+    (playerSelection !== "rock") &&
+    (playerSelection !== "paper") &&
+    (playerSelection !== "scissors")
+  ) {
+    console.log("Invalid input.");
+    playerSelection = prompt("Enter rock, paper, or scissors:").toLowerCase();
+    if (
+      (playerSelection === "rock") ||
+      (playerSelection === "paper") ||
+      (playerSelection === "scissors")
     ) {
-      console.log("Invalid input.");
-      playerSelection = prompt("Enter rock, paper, or scissors:").toLowerCase();
-      if (
-        (playerSelection === "rock") ||
-        (playerSelection === "paper") ||
-        (playerSelection === "scissors")
-      ) {
-        break;
-      }
+      break;
     }
-    console.log(playRound(playerSelection, computerSelection));
   }
+  console.log(playRound(playerSelection, computerSelection));
+  // }
 }
 
-game();
+// game();
